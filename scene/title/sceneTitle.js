d8f5ac112620624f12bc38006f7936881010bfa5
wuxiaoqing997 <wuxiaoqing997@sina.com>
2018-03-06 13:38:25 +0800

    'fix:增加玩法提醒'

--- scene/title/sceneTitle.js
+++ scene/title/sceneTitle.js
@@ -8,7 +8,7 @@ class SceneTitle extends mainScene {
         })
     }
     draw() {
-        this.game.context.fillText('Press K Ready', 200, 200)
+        this.game.context.fillText('按k开始游戏，按f发射，a、d是左右移动，鼠标左键可增加砖块', 100, 200)
     }
     update() {}
 
